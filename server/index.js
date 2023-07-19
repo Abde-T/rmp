44a26d4aa339c1f21b49a6970b90a9b2b2777858
Abderrahmane <tiamani3939@gmail.com>
2023-07-19 15:48:09 +0100

    backend deployment

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ dotenv.config();
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(cors({
-  origin: "https://rmp-abde-t.vercel.app/",
+  origin: "https://rmp-abde-t.vercel.app",
   methods:['POST', 'GET'],
   credentials: true
 }));
