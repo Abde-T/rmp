8c905e174f2667d4706519d7cfe81395cf380e3f
Abderrahmane <tiamani3939@gmail.com>
2023-07-19 15:58:53 +0100

    backend deployment

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
+axios.defaults.withCredentials = true
 const API = axios.create({ baseURL: "https://rmp-api.vercel.app" });
-
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
     req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
