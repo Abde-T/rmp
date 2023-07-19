e2a466276af60efcb493fa6fee0d2c26129e4ef7
Abderrahmane <tiamani3939@gmail.com>
2023-07-19 15:50:08 +0100

    backend deployment

--- server/index.js
+++ server/index.js
@@ -29,8 +29,6 @@ const PORT = process.env.PORT || 3000;
 const startServer = async () => {
   try {
     connectDB(CONNECTION_URL);
-
-    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
   } catch (error) {
     console.log(error);
   }
