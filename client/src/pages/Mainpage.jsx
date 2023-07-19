d996bdbf3dc778286940f6212c7d2f63397f3fb3
Abderrahmane <tiamani3939@gmail.com>
2023-07-19 15:42:27 +0100

    backend deployment

--- client/src/pages/Mainpage.jsx
+++ client/src/pages/Mainpage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Landing from '../MainPageComponents/Landing';
-import Info from '../MainPageComponents/info';
+import Info from '../MainPageComponents/Info';
 import Details from '../MainPageComponents/Details';
 import FormSection from '../MainPageComponents/FormSection';
 import Footer from '../MainPageComponents/Footer';
