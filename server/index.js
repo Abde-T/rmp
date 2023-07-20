d067727eb68ead30ffd7ecf9a167f1504e310d3c
Abderrahmane <tiamani3939@gmail.com>
2023-07-20 09:41:44 +0100

    backend deployment

--- server/index.js
+++ server/index.js
@@ -13,22 +13,17 @@ dotenv.config();
 
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(cors({
-  origin: "https://rmp-abde-t.vercel.app",
-  methods:['POST', 'GET'],
-  credentials: true
-}));
+app.use(cors());
 app.use(cacheMiddleware())
 
 app.use("/posts", postRoutes);
 app.use("/user", userRoutes);
 
-const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 3000;
 
 const startServer = async () => {
   try {
-    connectDB(CONNECTION_URL);
+    connectDB(process.env.CONNECTION_URL);
 
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
   } catch (error) {
