2f599fb0e90a07966aecd06180c8452676628ec2
Abderrahmane <tiamani3939@gmail.com>
2023-12-17 10:23:23 +0100

    changes

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_BASE_URL =  process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000'
+const API_BASE_URL =  "https://rmpapi.onrender.com" || 'http://localhost:3000'
 const API = axios.create({ baseURL: API_BASE_URL});
 
 API.interceptors.request.use((req) => {
